5367db6b6380b73e66b3e3ac6dca3f4cdb421e82
TinoSolomon <72004138+TinoSolomon@users.noreply.github.com>
2024-02-29 13:57:27 -0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,22 @@
+#This file was created by: Tino Solomon
+
+TITLE = "my fun game"
+
+FPS = 60
+
+PLAYER_SPEED = 300
+
+#this is 32 grids wide
+WIDTH = 1024
+#this is 24 grids high
+HEIGHT = 768
+TILESIZE = 32
+
+SKYBLUE = (111, 186, 234)
+BGCOLOR = (SKYBLUE)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+PURPLE = (255, 0, 255)
+BLUE = (0, 0, 255)
